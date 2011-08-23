744a03849305a7936741b92dbdc1cc7a7a67802a
Daisuke Miyakawa <dmiyakawa@google.com>
2011-08-23 09:19:49 -0700

    Use ICS release name instead of version code
    Bug: 5148810
    Change-Id: I1b4d5d6280fb94e8cd514e3379aecee888eba479

--- common/java/com/android/common/contacts/DataUsageStatUpdater.java
+++ common/java/com/android/common/contacts/DataUsageStatUpdater.java
@@ -203,8 +203,9 @@ public class DataUsageStatUpdater {
         // From ICS we can use per-contact-method structure. We'll check if the device supports it
         // and call the API.
         //
-        // STOPSHIP: Use 14 or later when we're sure ICS has correct SDK version
-        if (Build.VERSION.SDK_INT >= 13) {
+        // STOPSHIP: Use Build.VERSION.SDK_INT when we're sure ICS has correct SDK version
+        // (bug 5047678)
+        if (TextUtils.equals("IceCreamSandwich", Build.VERSION.RELEASE)) {
             if (dataIds.isEmpty()) {
                 if (Log.isLoggable(TAG, Log.DEBUG)) {
                     Log.d(TAG, "Given list for data IDs is null. Ignoring.");
