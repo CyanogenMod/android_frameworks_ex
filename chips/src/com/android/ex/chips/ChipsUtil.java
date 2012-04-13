a0dfbe7734af1ae8a0fc2d9d9aefb6d8da5398e3
Daniel Lehmann <lehmannd@google.com>
2012-04-13 10:50:34 -0700

    Removed ChipsUtil's increment features which are not ICS-and-up safe
    Change-Id: Iaa05940c789ddfd4f37e5deb9e8e84c5cc28926f

--- chips/src/com/android/ex/chips/ChipsUtil.java
+++ chips/src/com/android/ex/chips/ChipsUtil.java
@@ -16,18 +16,7 @@
 
 package com.android.ex.chips;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
 import android.os.Build;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.Data;
-import android.text.TextUtils;
-import android.widget.MultiAutoCompleteTextView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 
 public class ChipsUtil {
 
@@ -36,60 +25,5 @@ public class ChipsUtil {
      */
     public static boolean supportsChipsUi() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
-    }
-
-    public static boolean tryUpdateRecencyInfo(MultiAutoCompleteTextView... views) {
-        for (MultiAutoCompleteTextView view : views) {
-            if (view instanceof RecipientEditTextView) {
-                updateRecencyInfo((RecipientEditTextView)view);
-            }
-        }
-        return true;
-    }
-
-    // TODO: check this works
-    public static void updateRecencyInfo(RecipientEditTextView view) {
-        final Context context = view.getContext();
-        final ContentResolver resolver = context.getContentResolver();
-        final long currentTimeMillis = System.currentTimeMillis();
-
-        final Collection<Long> contactIds = view.getContactIds();
-        if (contactIds != null) {
-            StringBuilder whereBuilder = new StringBuilder();
-            ArrayList<String> whereArgs = new ArrayList<String>();
-            String[] questionMarks = new String[contactIds.size()];
-            for (Long contactId : contactIds) {
-                whereArgs.add(String.valueOf(contactId));
-            }
-            Arrays.fill(questionMarks, "?");
-            whereBuilder.append(ContactsContract.Contacts._ID + " IN (").
-                    append(TextUtils.join(",", questionMarks)).
-                    append(")");
-
-            ContentValues values = new ContentValues();
-            values.put(ContactsContract.Contacts.LAST_TIME_CONTACTED,
-                    System.currentTimeMillis());
-            resolver.update(ContactsContract.Contacts.CONTENT_URI, values,
-                    whereBuilder.toString(), whereArgs.toArray(new String[0]));
-        }
-
-        final Collection<Long> dataIds = view.getDataIds();
-        if (dataIds != null) {
-            StringBuilder whereBuilder = new StringBuilder();
-            ArrayList<String> whereArgs = new ArrayList<String>();
-            String[] questionMarks = new String[dataIds.size()];
-            for (Long dataId : dataIds) {
-                whereArgs.add(String.valueOf(dataId));
-            }
-            Arrays.fill(questionMarks, "?");
-            whereBuilder.append(ContactsContract.Data._ID + " IN (").
-            append(TextUtils.join(",", questionMarks)).
-            append(")");
-
-            final ContentValues values = new ContentValues();
-            values.put("last_time_contacted", currentTimeMillis);
-            resolver.update(Data.CONTENT_URI, values,
-                    whereBuilder.toString(), whereArgs.toArray(new String[0]));
-        }
     }
 }
