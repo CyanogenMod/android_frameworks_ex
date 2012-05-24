c75bea26b81d1075fac128188a594aa511d94d9b
Mindy Pereira <mindyp@google.com>
2012-05-24 13:53:29 -0700

    Dont hide the keyboard when showing alternates/ selecting a chip.
    Fixes b/5557044 Chips replace-email-window shouldn't close the keyboard
    
    Change-Id: I93829983346de77ee79dd602ae84152d262ce01d

--- chips/src/com/android/ex/chips/RecipientEditTextView.java
+++ chips/src/com/android/ex/chips/RecipientEditTextView.java
@@ -76,7 +76,6 @@ import android.widget.ListAdapter;
 import android.widget.ListPopupWindow;
 import android.widget.ListView;
 import android.widget.MultiAutoCompleteTextView;
-import android.widget.PopupWindow;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -97,7 +96,7 @@ import java.util.regex.Matcher;
 public class RecipientEditTextView extends MultiAutoCompleteTextView implements
         OnItemClickListener, Callback, RecipientAlternatesAdapter.OnCheckedItemChangedListener,
         GestureDetector.OnGestureListener, OnDismissListener, OnClickListener,
-        PopupWindow.OnDismissListener, TextView.OnEditorActionListener {
+        TextView.OnEditorActionListener {
 
     private static final char COMMIT_CHAR_COMMA = ',';
 
@@ -228,16 +227,13 @@ public class RecipientEditTextView extends MultiAutoCompleteTextView implements
             sSelectedTextColor = context.getResources().getColor(android.R.color.white);
         }
         mAlternatesPopup = new ListPopupWindow(context);
-        mAlternatesPopup.setOnDismissListener(this);
         mAddressPopup = new ListPopupWindow(context);
-        mAddressPopup.setOnDismissListener(this);
         mCopyDialog = new Dialog(context);
         mAlternatesListener = new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView,View view, int position,
                     long rowId) {
                 mAlternatesPopup.setOnItemClickListener(null);
-                setEnabled(true);
                 replaceChip(mSelectedChip, ((RecipientAlternatesAdapter) adapterView.getAdapter())
                         .getRecipientEntry(position));
                 Message delayed = Message.obtain(mHandler, DISMISS);
@@ -1348,7 +1344,6 @@ public class RecipientEditTextView extends MultiAutoCompleteTextView implements
         // Align the alternates popup with the left side of the View,
         // regardless of the position of the chip tapped.
         alternatesPopup.setWidth(width);
-        setEnabled(false);
         alternatesPopup.setAnchorView(this);
         alternatesPopup.setVerticalOffset(bottom);
         alternatesPopup.setAdapter(createAlternatesAdapter(currentChip));
@@ -1366,12 +1361,6 @@ public class RecipientEditTextView extends MultiAutoCompleteTextView implements
             listView.setItemChecked(mCheckedItem, true);
             mCheckedItem = -1;
         }
-    }
-
-    // Dismiss listener for alterns and single address popup.
-    @Override
-    public void onDismiss() {
-        setEnabled(true);
     }
 
     private ListAdapter createAlternatesAdapter(RecipientChip chip) {
@@ -1895,7 +1884,6 @@ public class RecipientEditTextView extends MultiAutoCompleteTextView implements
         int bottom = calculateOffsetFromBottom(line);
         // Align the alternates popup with the left side of the View,
         // regardless of the position of the chip tapped.
-        setEnabled(false);
         popup.setWidth(width);
         popup.setAnchorView(this);
         popup.setVerticalOffset(bottom);
