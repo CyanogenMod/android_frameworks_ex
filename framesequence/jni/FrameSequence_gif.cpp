ea108679714bd53ef43b463b5a7a3d4b516d38c7
Chris Craik <ccraik@google.com>
2016-10-18 10:17:41 -0700

    Handle color bounds correctly in GIF decode.
    CYNGNOS-3312
    Bug: 31631842
    Change-Id: Ia4d25d9cd440286366f82433d3ff227384c0572f
    (cherry picked from commit 0ada9456d0270cb0e357a43d9187a6418d770760)

--- framesequence/jni/FrameSequence_gif.cpp
+++ framesequence/jni/FrameSequence_gif.cpp
@@ -157,7 +157,7 @@ static bool checkIfCover(const GifImageDesc& target, const GifImageDesc& covered
 static void copyLine(Color8888* dst, const unsigned char* src, const ColorMapObject* cmap,
                      int transparent, int width) {
     for (; width > 0; width--, src++, dst++) {
-        if (*src != transparent) {
+        if (*src != transparent && *src < cmap->ColorCount) {
             *dst = gifColorToColor8888(cmap->Colors[*src]);
         }
     }
